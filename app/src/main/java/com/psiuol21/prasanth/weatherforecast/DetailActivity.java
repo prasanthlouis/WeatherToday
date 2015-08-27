a5bd8f300ce1e9cca6c05f36a9aa026140fd5ed1
prasanthlouis <prasanthlouis21@gmail.com>
2015-08-27 13:04:16 -0400

    Shared Action Provider

--- app/src/main/java/com/psiuol21/prasanth/weatherforecast/DetailActivity.java
+++ app/src/main/java/com/psiuol21/prasanth/weatherforecast/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.psiuol21.prasanth.weatherforecast;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -22,7 +21,8 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
+
+                return true;
     }
 
     @Override
@@ -30,14 +30,11 @@ public class DetailActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent=new Intent(this,SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/psiuol21/prasanth/weatherforecast/DetailActivityFragment.java
+++ app/src/main/java/com/psiuol21/prasanth/weatherforecast/DetailActivityFragment.java
@@ -1,10 +1,17 @@
 package com.psiuol21.prasanth.weatherforecast;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.TextView;
 
 
@@ -12,8 +19,18 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-
+public static final String Log_tag=DetailActivityFragment.class.getSimpleName();
+    public static final String Forecast_Hashtag="#WeatherForecast";
+            public String mForecast;
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
+    }
+    private Intent createshareintent(){
+        Intent shareintent=new Intent(Intent.ACTION_SEND);
+        shareintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareintent.setType("text/plain");
+        shareintent.putExtra(Intent.EXTRA_TEXT, mForecast + Forecast_Hashtag);
+        return shareintent;
     }
 
     @Override
@@ -21,9 +38,40 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_detail, container, false);
         String forecast=getActivity().getIntent().getStringExtra("Forecast");
+        mForecast=forecast;
         TextView tv=(TextView)view.findViewById(R.id.TextView1);
-        tv.setText(forecast);
+        tv.setText(mForecast);
 // TODO Check if this works
         return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent=new Intent(getActivity(),SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.detailfargment,menu);
+        MenuItem menuItem=menu.findItem(R.id.action_share);
+        ShareActionProvider sap=(ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if(sap!=null)
+        {
+            sap.setShareIntent(createshareintent());
+        }
+
     }
 }
